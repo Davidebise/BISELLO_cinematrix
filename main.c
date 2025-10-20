94a29ae12bb7565af755ac5535b1eca84f61cae7
Davide <12999297@itisrossi.vi.it>
2025-10-20 13:25:09 +0200

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct {
+    char *titolo;
+    char *regista;
+    int anno;
+    float durata;
+}Film;
+int file_exists(char *filename) {
+    FILE *fileBinario= fopen(filename,"rb");
+    if (fileBinario == NULL) {
+        return 0;
+    }else {
+        return 1;
+    }
+}
 int main(void) {
-    printf("Hello, World!\n");
+    int dimensione=4;
+
+    Film *elenco;
+    if (file_exists("canzoni.dat")) {
+        FILE *file=fopen("canzoni.dat","rb");
+        fread(&dimensione,sizeof(int),1,file);
+        fread(elenco,sizeof(Film),dimensione,file);
+    }else {
+
+    }
     return 0;
 }
